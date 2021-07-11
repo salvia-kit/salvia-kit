5847a0704e444e8ccfa230057c704428c873a0a4
enochndika <enochbeloved@gmail.com>
2021-07-11 21:19:48 +0100

    add data in array of objects

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -2,6 +2,24 @@ import DocV1Vue from '@/src/docs/v1-2/vue/doc';
 import DocV1Nuxt from '@/src/docs/v1-2/nuxt/doc';
 import DocV1Next from '@/src/docs/v1-2/next/doc';
 import DocV1React from '@/src/docs/v1-2/react/doc';
+import DocV3React from '@/src/docs/v3/react/doc';
+import DocV3Next from '@/src/docs/v3/next/doc';
+import DocV3Vue from '@/src/docs/v3/vue/doc';
+import DocV3Nuxt from '@/src/docs/v3/nuxt/doc';
+import DocV4React from '@/src/docs/v4-5/react/doc';
+import DocV4Next from '@/src/docs/v4-5/next/doc';
+import DocV4Vue from '@/src/docs/v4-5/vue/doc';
+import DocV4Nuxt from '@/src/docs/v4-5/nuxt/doc';
+import DocV6React from '@/src/docs/v6/react/doc';
+import DocV6Next from '@/src/docs/v6/next/doc';
+import DocV6Vue from '@/src/docs/v6/vue/doc';
+import DocV6Nuxt from '@/src/docs/v6/nuxt/doc';
+import DocV7React from '@/src/docs/v7/react/doc';
+import DocV7Next from '@/src/docs/v7/next/doc';
+import DocV7Vue from '@/src/docs/v7/vue/doc';
+import DocV7Nuxt from '@/src/docs/v7/nuxt/doc';
+import DocV8React from '@/src/docs/v8/react/doc';
+import DocV8Next from '@/src/docs/v8/next/doc';
 
 const data = [
   {
@@ -45,6 +63,130 @@ const data = [
       nuxt: {
         demo: '',
         doc: <DocV1Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V3',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV3React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV3Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV3Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV3Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V4',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV4React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV4Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV4Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV4Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V5',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV4React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV4Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV4Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV4Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V6',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV6React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV6Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV6Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV6Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V7',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV7React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV7Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV7Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV7Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V8',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV8React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV8Next />,
       },
     },
   },
