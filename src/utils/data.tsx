34c477afd09b2b0d02e7997f36b26042c3ea9b2c
enochndika <enochbeloved@gmail.com>
2021-07-11 03:23:02 +0100

    add dashboards data in an array od objects

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -1,3 +1,8 @@
+import DocV1Vue from '@/src/docs/v1-2/vue/doc';
+import DocV1Nuxt from '@/src/docs/v1-2/nuxt/doc';
+import DocV1Next from '@/src/docs/v1-2/next/doc';
+import DocV1React from '@/src/docs/v1-2/react/doc';
+
 const data = [
   {
     name: 'Salvia-kit Dashboard V1',
@@ -5,21 +10,44 @@ const data = [
     techno: {
       react: {
         demo: '',
-        doc: '',
+        doc: <DocV1React />,
       },
       next: {
         demo: '',
-        doc: '',
+        doc: <DocV1Next />,
       },
       vue: {
         demo: '',
-        doc: '',
+        doc: <DocV1Vue />,
       },
       nuxt: {
         demo: '',
-        doc: '',
+        doc: <DocV1Nuxt />,
+      },
+    },
+  },
+  {
+    name: 'Salvia-kit Dashboard V2',
+    image: '',
+    techno: {
+      react: {
+        demo: '',
+        doc: <DocV1React />,
+      },
+      next: {
+        demo: '',
+        doc: <DocV1Next />,
+      },
+      vue: {
+        demo: '',
+        doc: <DocV1Vue />,
+      },
+      nuxt: {
+        demo: '',
+        doc: <DocV1Nuxt />,
       },
     },
   },
 ];
+
 export default data;
