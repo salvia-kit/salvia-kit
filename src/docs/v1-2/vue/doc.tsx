9ef1243eed273e552dd0b59fc3bbafcdf5a5ba1b
enochndika <enochbeloved@gmail.com>
2021-07-11 16:14:18 +0100

    refactor doc v1 vue

--- src/docs/v1-2/vue/doc.tsx
+++ src/docs/v1-2/vue/doc.tsx
@@ -5,7 +5,7 @@ import V1VueThirdSection from '@/src/docs/v1-2/vue/sections/third';
 import V1VueFourthSection from '@/src/docs/v1-2/vue/sections/fourth';
 import V1VueFifthSection from '@/src/docs/v1-2/vue/sections/fifth';
 import V1VueSixthSection from '@/src/docs/v1-2/vue/sections/sixth';
-import V1VueSeventhSection from '@/src/docs/v1-2/vue/sections/seventh';
+import LintersSection from '@/src/docs/common/linters';
 
 // this doc is for the version 1 and 2 of dashboard
 export default function DocV1Vue() {
@@ -19,9 +19,9 @@ export default function DocV1Vue() {
       <V1VueSecondSection />
       <V1VueThirdSection />
       <V1VueFourthSection />
+      <LintersSection />
       <V1VueFifthSection />
       <V1VueSixthSection />
-      <V1VueSeventhSection />
     </div>
   );
 }
