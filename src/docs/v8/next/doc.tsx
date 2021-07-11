d804d7884e374a556bcc2678ca8a921cf8e40a54
enochndika <enochbeloved@gmail.com>
2021-07-11 21:11:45 +0100

    add doc for dashboard v8

---
+++ src/docs/v8/next/doc.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import V8NextFirstSection from '@/src/docs/v8/next/sections/first';
+import V8NextSecondSection from '@/src/docs/v8/next/sections/second';
+import V8NextThirdSection from '@/src/docs/v8/next/sections/third';
+import V8NextFourthSection from '@/src/docs/v8/next/sections/fourth';
+import V8NextFifthSection from '@/src/docs/v8/next/sections/fifth';
+import LintersSection from '@/src/docs/common/linters';
+import ComponentsSection from '@/src/docs/common/components';
+import FeaturesSection from '@/src/docs/common/features';
+
+export default function DocV8Next() {
+  return (
+    <div className="py-8 max-w-full text-gray-700">
+      <h2 className="flex mb-16 text-3xl font-medium">
+        <Image
+          src="/svg/nextjs.svg"
+          height={70}
+          width={70}
+          alt="Next.js icon"
+        />
+        <span className="pl-2 pt-4">Documentation</span>
+      </h2>
+      <FeaturesSection />
+      <V8NextFirstSection />
+      <V8NextSecondSection />
+      <V8NextThirdSection />
+      <LintersSection />
+      <V8NextFourthSection />
+      <V8NextFifthSection />
+      <ComponentsSection />
+    </div>
+  );
+}
---
+++ src/docs/v8/react/doc.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import V8ReactFirstSection from '@/src/docs/v8/react/sections/first';
+import V8ReactSecondSection from '@/src/docs/v8/react/sections/second';
+import V8ReactThirdSection from '@/src/docs/v8/react/sections/third';
+import V8ReactFourthSection from '@/src/docs/v8/react/sections/fourth';
+import V8ReactFifthSection from '@/src/docs/v8/react/sections/fifth';
+import ComponentsSection from '@/src/docs/common/components';
+import LintersSection from '@/src/docs/common/linters';
+import FeaturesSection from '@/src/docs/common/features';
+
+export default function DocV8React() {
+  return (
+    <div className="py-8 max-w-full text-gray-700">
+      <h2 className="flex mb-16 text-3xl font-medium">
+        <Image src="/svg/react.svg" height={30} width={30} alt="React icon" />
+        <span className="pl-2">Documentation</span>
+      </h2>
+      <FeaturesSection />
+      <V8ReactFirstSection />
+      <V8ReactSecondSection />
+      <V8ReactThirdSection />
+      <LintersSection />
+      <V8ReactFourthSection />
+      <V8ReactFifthSection />
+      <ComponentsSection />
+    </div>
+  );
+}
