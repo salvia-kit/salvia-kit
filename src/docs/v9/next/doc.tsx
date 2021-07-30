4d115e22942364e8bcbf1a389d13304ff07d32de
enochndika <enochbeloved@gmail.com>
2021-07-30 05:21:42 +0100

    update dashboard v9 docs for Next.js

--- src/docs/v9/next/doc.tsx
+++ src/docs/v9/next/doc.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 import LintersSection from '@/src/docs/common/linters';
 import ComponentsSection from '@/src/docs/common/components';
 import V9NextFirstSection from '@/src/docs/v9/next/sections/first';
@@ -7,13 +5,14 @@ import V9NextThirdSection from '@/src/docs/v9/next/sections/third';
 import V9ReactNextFeaturesSection from '@/src/docs/common/section/react-next/features';
 import { V9ReactNextFifthSection } from '@/src/docs/common/section/react-next/fifthSection';
 import { ReactNextV9SecondSection } from '@/src/docs/common/section/react-next/secondSection';
+import NextIcon from '@/src/components/icons/next';
 
 export default function DocV9Next() {
   return (
     <div className="mt-12 max-w-full">
       <h2 className="flex mb-16 text-3xl font-medium">
-        <Image src="/svg/react.svg" height={30} width={30} alt="React icon" />
-        <span className="pl-2">Documentation</span>
+        <NextIcon className="mt-2 w-20 h-12" />
+        <span className="pl-2 pt-4">Documentation</span>
       </h2>
       <V9ReactNextFeaturesSection />
       <V9NextFirstSection />
