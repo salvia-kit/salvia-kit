9dd09b6bbc1f97e5a0e7ff14d046ef733aad40d1
enochndika <enochbeloved@gmail.com>
2021-07-30 05:03:02 +0100

    update home page second section content

--- src/components/home/sections/second.tsx
+++ src/components/home/sections/second.tsx
@@ -4,9 +4,9 @@ import DashboardCard from '@/src/components/dashboardCard';
 const HomeSecondSection = () => (
   <section className="mb-16 md:mb-32">
     <h2 className="text-center text-3xl font-bold">
-      Discover Our
+      Discover Our Current
       <br />
-      Dashboards Trends
+      Trends
     </h2>
     <div className="flex flex-wrap mt-12">
       {data.slice(3, 6).map((dash) => (
